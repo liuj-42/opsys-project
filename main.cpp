1e78e2e9d2df6682dc59910000ad2f59606db347
James Liu <liuxjames88@gmail.com>
2022-07-08 18:58:48 -0400

    working on fcfs

--- main.cpp
+++ main.cpp
@@ -2,25 +2,61 @@
 #include <fstream>
 #include <string>
 
+#include "next_exp.cpp"
+
+
+std::string alphabet[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
 int main( int argc, char** argv ) {
   if ( argc != 8 ) {
     std::cerr << "invalid arguments" << std::endl;
     return EXIT_FAILURE;
   }
-  int n = atoi( argv[1] );
-  int seed = atoi( argv[2] );
-  float lambda = atof( argv[3] );
-  float upperBound = atof( argv[4] );
-  float contextSwitch = atof( argv[5] );
-  float alpha = atof( argv[6] );
-  float timeSlice = atof( argv[7] );
+  int numProcesses = atoi(*(argv + 1));
+  int seed = atoi(*(argv + 2));
+  double lambda = strtod(*(argv + 3), NULL);
+  int upperBound = atoi(*(argv + 4));
+  float contextSwitch = atof( *(argv + 5) );
+  float alpha = atof( *(argv + 6) );
+  float timeSlice = atof( *(argv + 7) );
 
   std::string names[7] = { "processes\t\t\t", "seed\t\t\t\t", "lambda\t\t\t\t", "upper bound\t\t\t", "context switch time\t", "alpha\t\t\t\t", "time slice\t\t\t" };
 
   for ( int i = 0; i < 7; i++ ) {
-    std::cout << names[i] << "| " << argv[i + 1] << std::endl;
+    std::cout << *(names + i) << "| " << *(argv + i + 1) << std::endl;
   }
 
-  // std::cout << "processes|\tseed|\tlambda|\tupper bound|\tcontext switch time|\talpha|\ttime slice" << std::endl;
-  // std::cout << n << "\t\t\t\t\t\t" << seed << "\t\t" << lambda << "\t\t" << upperBound << "\t\t\t\t\t" << contextSwitch << "\t\t\t\t\t\t\t\t\t\t\t" << alpha << "\t\t\t" << timeSlice << std::endl;
+  std::cout << "\n\n------------------------------\n";
+
+  srand48(seed);
+  
+  std::vector<int> arrivalTimes;
+  std::vector<int> cpuBurstsNums;
+  std::vector<std::vector<CPUBurst>> cpuBursts;
+
+  for(int i = 0; i < numProcesses; i++) generate_process(i, seed, lambda, upperBound, arrivalTimes, cpuBurstsNums, cpuBursts);
+
+  int time = arrivalTimes[0];
+
+  for(int i = 0; i < numProcesses; i++) {
+    // std::cout << "Process ID: " << i << "\n";
+    // std::cout << "Arrival Time: " << arrivalTimes[i] << "ms\n";
+    // std::cout << "Num. of CPU Bursts: " << cpuBurstsNums[i] << "\n";
+    // std::cout << "CPU Bursts: \n\n";
+    std::cout << "time 0ms: Simulator started for FCFS [Q: empty]" << std::endl;
+    std::cout << "time " << time << "ms: Process " << alphabet[i] << " arrived; added to ready queue [Q: " << alphabet[i] << "]\n";
+
+    for(int c = 0; c < cpuBurstsNums[i]; c++) {
+      // std::cout << "CPU Burst #" << c << "\n";
+      // std::cout << "CPU Burst Time: " << cpuBursts[i][c].cpu_burst_time << "ms\n";
+      // std::cout << "I/O Burst Time: " << cpuBursts[i][c].io_burst_time << "ms\n";
+      // std::cout << "\n";
+      int cpu = cpuBursts[i][c].cpu_burst_time;
+      int io = cpuBursts[i][c].io_burst_time;
+      time += cpu + io + contextSwitch;
+    }
+  }
+
+  std::cout<< "total time: " << time << std::endl;
+
 }
--- next_exp.cpp
+++ next_exp.cpp
@@ -57,32 +57,33 @@ void generate_process(int id, int seed, double lambda, int upper_bound,
  * agrv[3] - lambda
  * argv[4] - upper bound
 */
-int main(int argc, char** argv) {
-  if(argc != 5) return EXIT_FAILURE;
-  int num_processes = atoi(*(argv + 1));
-  int seed = atoi(*(argv + 2));
-  double lambda = strtod(*(argv + 3), NULL);
-  int upper_bound = atoi(*(argv + 4));
+// int main(int argc, char** argv) {
+//   if(argc != 5) return EXIT_FAILURE;
+//   int num_processes = atoi(*(argv + 1));
+//   int seed = atoi(*(argv + 2));
+//   double lambda = strtod(*(argv + 3), NULL);
+//   int upper_bound = atoi(*(argv + 4));
 
-  srand48(seed);
+//   srand48(seed);
   
-  std::vector<int> arrival_times;
-  std::vector<int> cpu_bursts_nums;
-  std::vector<std::vector<CPUBurst>> cpu_bursts;
+//   std::vector<int> arrival_times;
+//   std::vector<int> cpu_bursts_nums;
+//   std::vector<std::vector<CPUBurst>> cpu_bursts;
 
-  for(int i = 0; i < num_processes; i++) generate_process(i, seed, lambda, upper_bound,
-    arrival_times, cpu_bursts_nums, cpu_bursts);
+//   for(int i = 0; i < num_processes; i++) generate_process(i, seed, lambda, upper_bound,
+//     arrival_times, cpu_bursts_nums, cpu_bursts);
 
-  for(int i = 0; i < num_processes; i++) {
-    std::cout << "Process ID: " << i << "\n";
-    std::cout << "Arrival Time: " << arrival_times[i] << "ms\n";
-    std::cout << "Num. of CPU Bursts: " << cpu_bursts_nums[i] << "\n";
-    std::cout << "CPU Bursts: \n\n";
-    for(int c = 0; c < cpu_bursts_nums[i]; c++) {
-      std::cout << "CPU Burst #" << c << "\n";
-      std::cout << "CPU Burst Time: " << cpu_bursts[i][c].cpu_burst_time << "ms\n";
-      std::cout << "I/O Burst Time: " << cpu_bursts[i][c].io_burst_time << "ms\n";
-      std::cout << "\n";
-    }
-  }
-}
+//   for(int i = 0; i < num_processes; i++) {
+//     std::cout << "Process ID: " << i << "\n";
+//     std::cout << "Arrival Time: " << arrival_times[i] << "ms\n";
+//     std::cout << "Num. of CPU Bursts: " << cpu_bursts_nums[i] << "\n";
+//     std::cout << "CPU Bursts: \n\n";
+//     for(int c = 0; c < cpu_bursts_nums[i]; c++) {
+//       std::cout << "CPU Burst #" << c << "\n";
+//       std::cout << "CPU Burst Time: " << cpu_bursts[i][c].cpu_burst_time << "ms\n";
+//       std::cout << "I/O Burst Time: " << cpu_bursts[i][c].io_burst_time << "ms\n";
+//       std::cout << "\n";
+//     }
+//   }
+// }
+
