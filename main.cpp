3d212e5a6db1a32166a20be2b42b02c071ff4227
James Liu <liuxjames88@gmail.com>
2022-07-09 17:35:27 -0400

    got fcfs to match with the first output

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "process.h"
 char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-int fcfs( std::vector<Process> processes );
+int fcfs( std::vector<Process> processes, int contextSwitch );
 
 int main( int argc, char** argv ) {
   if ( argc != 8 ) {
@@ -38,52 +38,64 @@ int main( int argc, char** argv ) {
   for ( int i = 0; i < numProcesses; i++ ) {
     processes.push_back( Process(alphabet[i], seed, lambda, upperBound) );
   }
+
+
   int time = 0;
   // FCFS
-  time += fcfs( processes );
-
-
-  // for(int i = 0; i < numProcesses; i++) {
-  //   // std::cout << "Process ID: " << i << "\n";
-  //   // std::cout << "Arrival Time: " << arrivalTimes[i] << "ms\n";
-  //   // std::cout << "Num. of CPU Bursts: " << cpuBurstsNums[i] << "\n";
-  //   // std::cout << "CPU Bursts: \n\n";
-  //   char pid = alphabet[i];
-  //   std::cout << "time 0ms: Simulator started for FCFS [Q: empty]" << std::endl;
-  //   std::cout << "time " << time << "ms: Process " << pid << " arrived; added to ready queue [Q: " << pid << "]\n";
-
-  //   for(int c = 0; c < cpuBurstsNums[i]; c++) {
-  //     // std::cout << "CPU Burst #" << c << "\n";
-  //     // std::cout << "CPU Burst Time: " << cpuBursts[i][c].cpu_burst_time << "ms\n";
-  //     // std::cout << "I/O Burst Time: " << cpuBursts[i][c].io_burst_time << "ms\n";
-  //     // std::cout << "\n";
-  //     time += contextSwitch/2;
-  //     int cpu = cpuBursts[i][c].cpu_burst_time;
-  //     int io = cpuBursts[i][c].io_burst_time;
-  //     std::cout << "time " << time << "ms: Process " << alphabet[i] << "started using the CPU for " << cpu << "ms burst [Q: empty]\n";
-  //     time += cpu;
-  //     std::cout << "time " << time << "ms: Process " << pid << " completed a CPU burst; " << cpuBurstsNums[i]-c << " bursts to go [Q: empty]\n";
-  //     std::cout << "time " << time << "ms: Process " << pid <<  " switching out of CPU; will block on I/O until time " << time + io << "ms [Q: empty]\n";
-  //     time += io;
-  //     std::cout << "time " << time << "ms: Process " << pid << " completed I/O; added to ready queue [Q: " << pid << "]\n";
-  //     time += contextSwitch/2;
-
-  //   }
-  // }
-
-  // std::cout<< "total time: " << time << std::endl;
+  time += fcfs( processes, contextSwitch );
 
 }
 
+std::string prefix( int time, char pid ) {
+  std::string out = "time ";
+  out += std::to_string(time);
+  out += "ms; Process ";
+  out += pid;
+  out += " ";
+  return out;
+}
 
-int fcfs( std::vector<Process> processes ) {
+// std::string
+
+int fcfs( std::vector<Process> processes, int contextSwitch ) {
   int time = 0;
+  std::vector<char> queue;
   for ( Process p : processes ) {
-    std::cout<< "new process " << p.getID() << std::endl;
+    std::cout << "time " << time << "ms: Simulator started for FCFS [Q: empty]\n";
+    time += p.getArrivalTime();
+    std::cout << prefix( time, p.getID() ) << "arrived, added to ready queue";
+    std::cout << "[Q: " << p.getID() << "]\n";
     std::list<std::pair<int, int>> bursts = p.getBursts();
     int index = 0;
     for ( auto burstItr = bursts.begin(); burstItr != bursts.end(); burstItr++, index++ ) {
-      std::cout << "CPU Burst: " << burstItr->first << "ms  \tIO Burst: " << burstItr->second << "ms\n";
+      time += contextSwitch/2;
+      std::cout << prefix( time, p.getID() ) << "started using the CPU for " << burstItr->first << "ms burst ";
+      std::cout << "[Q: empty]\n";
+      time += burstItr->first;
+      std::cout << prefix( time, p.getID() ) << "completed a CPU burst; " << p.getRemainingBursts(index) << (p.getRemainingBursts(index) == 1 ? " burst to go " : " bursts to go ");  
+      std::cout << "[Q: empty]\n";
+
+      if ( burstItr->second ) {
+        std::cout << prefix( time, p.getID() ) << "switching out of CPU, will block on I/O until time " << time + burstItr->second << "ms ";
+        std::cout << "[Q: empty]\n";
+        time += burstItr->second;
+        time += contextSwitch/2;
+        std::cout << prefix( time, p.getID() )  << "completed I/O; added to ready queue ";
+        std::cout << "[Q: " << p.getID() << "]\n";
+      }
+
+
+
+      
+      // std::cout << "CPU Burst: " << burstItr->first << "ms  \tIO Burst: " << burstItr->second << "ms\n";
     }
+    std::cout << prefix( time, p.getID() ) << "terminated ";
+    std::cout << "[Q: empty]\n";
   }
+
+  std::cout << "time " << time << "ms: Simulator ended for FCFS [Q: empty]\n";
+  // std::cout << "total time: " << time << "ms\n";
+
+  return time;
 }
+
--- process.h
+++ process.h
@@ -21,38 +21,37 @@ public:
         pid = id;
         arrival_time = floor(next_exp(seed, lambda, upper_bound));
         cpu_bursts_num = ceil(next_unif(seed));
+        std::cout << "Process " << pid << ": arrival time " << arrival_time << "ms; tau " << "TBD" << "ms; " << cpu_bursts_num << " CPU bursts:" << std::endl;
         for (int c = 0; c < cpu_bursts_num; c++)
         {   
             int cpuBurst = ceil(next_exp(seed, lambda, upper_bound));
+            std::cout << "--> CPU burst " << cpuBurst << "ms";
             int ioBurst = 0;
             if (c != cpu_bursts_num - 1)
             {   
                 ioBurst = ceil(next_exp(seed, lambda, upper_bound));
                 ioBurst *= 10;
+                std::cout << " --> I/O burst " << ioBurst << "ms";
             }
+            std::cout << std::endl;
+            
             std::pair<int, int> burst(cpuBurst, ioBurst);
             bursts.push_back(burst);
         }
-        burstItr = bursts.begin();
-    }
-
-    std::pair<int, int> nextBurst() {
-        return *(burstItr++);
+        std::cout << std::endl;
     }
 
     // getters
     char getID() { return pid; }
     int getArrivalTime() { return arrival_time; }
     int getBurstsNum() { return cpu_bursts_num; }
+    int getRemainingBursts( int index ) { return cpu_bursts_num - index; }
     const std::list<std::pair<int, int>> getBursts() { return bursts; }
-
 
     // debug
     void printAllBursts() {
         auto burstItr = bursts.begin();
         while( burstItr != bursts.end() ) {
-            std::pair<int, int> burst = nextBurst();
-            std::cout << "CPU Burst: " << burst.first << "ms  \tIO Burst: " << burst.second << "ms\n";
             std::cout << "CPU Burst: " << burstItr->first << "ms  \tIO Burst: " << burstItr++->second << "ms\n";
         }
     }
@@ -66,9 +65,8 @@ private:
     int cpu_bursts_num; // Number of bursts
     // burst.first is CPU burst time
     // burst.second is IO burst time
-    // std::vector<std::pair<int, int>> bursts;
+    // std::list<std::pair<int, int>> bursts;
     std::list<std::pair<int, int>> bursts;
-    std::list<std::pair<int, int>>::iterator burstItr;
 };
 
 
